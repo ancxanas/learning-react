c9f35e65c194b2cced0a2bfe11d0d4f15145d1ac
ancxanas <anas.corona.github@gmail.com>
2023-01-25 13:57:01 +0530

    Refactored the application to its own module

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     commonjs: true,
     es2021: true,
   },
@@ -14,7 +14,7 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    eqeqaq: 'error',
+    eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
