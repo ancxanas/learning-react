17077a80a39af06016a93a543e12d2363f4ca7f3
ancxanas <anas.corona.github@gmail.com>
2023-02-15 22:31:37 +0530

    Added login

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,43 +1,62 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const mongoose = require('mongoose');
+const isValidObjectId = mongoose.isValidObjectId;
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
+  response.json(notes);
 });
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(400).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.get('/:id', async (request, response) => {
+  if (!isValidObjectId(request.params.id)) {
+    return response.status(400).end();
+  }
+
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
 });
 
-notesRouter.post('/', (request, response, next) => {
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  } else {
+    return null;
+  }
+};
+
+notesRouter.post('/', async (request, response) => {
   const body = request.body;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
+    important: body.important === undefined ? false : body.important,
+    user: user._id,
   });
 
-  note
-    .save()
-    .then((savedNote) => response.json(savedNote))
-    .catch((error) => next(error));
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.status(201).json(savedNote);
 });
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => response.status(204).end())
-    .catch((error) => next(error));
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 notesRouter.put('/:id', (request, response, next) => {
--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,46 +1,197 @@
-const mongoose = require('mongoose');
 const supertest = require('supertest');
+const mongoose = require('mongoose');
+const helper = require('./test_helper');
 const app = require('../app');
 const api = supertest(app);
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
 const Note = require('../models/note');
 
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-];
+let token = '';
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(initialNotes[1]);
-  await noteObject.save();
+  await Note.insertMany(helper.initialNotes);
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash('sekret', 10);
+  const user = new User({
+    username: 'root',
+    passwordHash,
+  });
+
+  await user.save();
+
+  const response = await api
+    .post('/api/login')
+    .send({
+      username: 'root',
+      password: 'sekret',
+    })
+    .expect(200);
+
+  token = response.body.token;
 });
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there is initially some notes saved', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/notes');
+
+    expect(response.body).toHaveLength(helper.initialNotes.length);
+  });
+
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes');
+
+    const contents = response.body.map((r) => r.content);
+    expect(contents).toContain('Browser can execute only JavaScript');
+  });
 });
 
-test('all notes are returned', async () => {
-  const response = await api.get('/api/notes');
+describe('viewing a specific note', () => {
+  test('succeeds with a valid id', async () => {
+    const notesAtStart = await helper.notesInDb();
 
-  expect(response.body).toHaveLength(initialNotes.length);
+    const noteToView = notesAtStart[0];
+
+    const resultNote = await api
+      .get(`/api/notes/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(resultNote.body).toEqual(noteToView);
+  });
+
+  test('fails with statuscode 404 if note does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
+
+    await api.get(`/api/notes/${validNonexistingId}`).expect(404);
+  });
+
+  test('fails with statuscode 400 if it is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445';
+
+    await api.get(`/api/notes/${invalidId}`).expect(400);
+  });
 });
 
-test('a specific note is within the returned notes', async () => {
-  const response = await api.get('/api/notes');
+describe('addition of a new note', () => {
+  test('succeeds with valid data', async () => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true,
+      user: '63e0e1d4e714c167b4a879ad',
+    };
 
-  const contents = response.body.map((r) => r.content);
-  expect(contents).toContain('Browser can execute only JavaScript');
+    await api
+      .post('/api/notes')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newNote)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const notesAtEnd = await helper.notesInDb();
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+
+    const contents = notesAtEnd.map((n) => n.content);
+
+    expect(contents).toContain('async/await simplifies making async calls');
+  });
+
+  test('fails with status code 400 if data invalid', async () => {
+    const newNote = {
+      important: true,
+    };
+
+    await api.post('/api/notes').send(newNote).expect(400);
+
+    const notesAtEnd = await helper.notesInDb();
+
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+  });
+});
+
+describe('deletion of a note', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const notesAtStart = await helper.notesInDb();
+
+    const noteToDelete = notesAtStart[0];
+
+    await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+    const notesAtEnd = await helper.notesInDb();
+
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+    const contents = notesAtEnd.map((r) => r.content);
+
+    expect(contents).not.toContain(noteToDelete.content);
+  });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({
+      username: 'root',
+      passwordHash,
+    });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const userAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Maatti Luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(userAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const userAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('expected `username` to be unique');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(userAtStart);
+  });
 });
 
 afterAll(async () => {
