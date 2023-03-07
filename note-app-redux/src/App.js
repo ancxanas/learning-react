a78451573334e35f90a323ebc3719f7ece9cc6aa
ancxanas <anas.corona.github@gmail.com>
2023-03-07 11:05:19 +0530

    useQuery

--- note-app-redux/src/App.js
+++ note-app-redux/src/App.js
@@ -1,8 +1,16 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
--- note-app-redux/src/index.js
+++ note-app-redux/src/index.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
-})
-
-const store = createStore(reducer)
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(
-  createNote('combineReducers forms one reducer from many simple reducers')
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
