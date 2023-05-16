98785589238561bcf9d0786ec51346fcbc1d21f2
ancxanas <anas.corona.github@gmail.com>
2023-05-16 20:29:26 +0530

    Added change number form

--- graphql-phonebook-frontend/src/App.js
+++ graphql-phonebook-frontend/src/App.js
@@ -1,21 +1,42 @@
 import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
 import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </>
   )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+
+  return <div style={{ color: 'red' }}>{errorMessage}</div>
 }
 
 export default App
