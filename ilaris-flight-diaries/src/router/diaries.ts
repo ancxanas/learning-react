f375ae5766471417a18d07dffa1ae1afaf0ee311
ancxanas <anas.corona.github@gmail.com>
2023-07-09 15:57:28 +0530

    Defined a router for fetching diary from id

--- ilaris-flight-diaries/src/router/diaries.ts
+++ ilaris-flight-diaries/src/router/diaries.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import diaryService from '../services/diaryService';
 
 const router = express.Router();
 
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.get('/', (_req, res) => {
-  res.send('Fetching all diaries!');
+  res.send(diaryService.getNonSensitiveEntries());
 });
 
 router.post('/', (_req, res) => {
---
+++ ilaris-flight-diaries/src/services/diaryService.ts
@@ -0,0 +1,27 @@
+import diaries from '../../data/diaries';
+
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
+
+export default { getEntries, addDiary, getNonSensitiveEntries, findById };
